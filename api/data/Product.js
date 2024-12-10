fd92a1709b9a4970ca4657b7e77f94103d06fba8
rahulupadhyay23 <rahulupadhyayknw@gmail.com>
2024-12-10 11:50:55 +0530

    connect nodejs with mongoose

---
+++ api/data/Product.js
@@ -0,0 +1,24 @@
+const products = [
+    {
+      "id":"1",
+      "name": "Stylish Silver Wireless Headphones",
+      "image": "https://images.pexels.com/photos/3755027/pexels-photo-3755027.jpeg",
+      "description": "Modern wireless headphones with minimalist design and premium sound quality.",
+      "price": 99.99,
+      "countInStock": 15,
+      "rating": 4.5,
+      "numReviews": 120
+    },
+    {
+      "id":"2",
+      "name": "Black Over-Ear Headphones",
+      "image": "https://images.unsplash.com/photo-1517649763962-0c623066013b",
+      "description": "Comfortable over-ear headphones with noise-canceling features.",
+      "price": 149.99,
+      "countInStock": 8,
+      "rating": 4.8,
+      "numReviews": 85
+    }
+  ];
+
+module.exports = products;
--- api/index.js
+++ api/index.js
@@ -2,13 +2,21 @@ const express = require("express");
 const app = express();
 
 const dotenv = require("dotenv");
+const products = require("./data/Product");
+const connectDB = require("./config/db")
 dotenv.config();
+connectDB();
 
 const PORT = process.env.PORT;
 
-//test route
-app.get("/", (req, res) => {
-    res.send("app is running")
+//api product test route
+app.get("/api/products", (req, res) => {
+    res.json({products})
+})
+
+app.get("/api/products/:id", (req, res) => {
+    const product = products.find((product) => product.id === req.params.id);
+    res.json(product)
 })
 
 app.listen(PORT || 9000, () => {
